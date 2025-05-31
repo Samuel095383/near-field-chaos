58a19b8b8fe39a0b97ff61042a611b9fee9299c4
Samuel095383 <105554130+Samuel095383@users.noreply.github.com>
2025-05-31 12:25:33 +0200

    Delete firmware directory

--- firmware/nfc/nfc_status.h
+++
@@ -1,3 +0,0 @@
-
-#define NFC_SUCCESS 0
-#define NFC_ERROR -1
--- firmware/utils/task.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stdio.h>
-#include "pico/stdlib.h"
-#include <pico/multicore.h>
-
-bool task_is_running();
-void task_execute();
-void task_stop();
-void task_start(void (*task)(void));
